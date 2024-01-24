1c5fff562a749bb404117e771921bafdc3867b1d
Michael <mihaerkhan2001@gmail.com>
2024-01-24 11:50:01 +0300

    add classes homework

---
+++ Classes_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Подготовка к бою:");
+
+        Console.WriteLine("Введите имя бойца:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите начальное здоровье бойца (10-100):");
+        int health = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение брони шлема от 0 до 1:");
+        float helmArmor = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение брони кирасы от 0 до 1:");
+        float shellArmor = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение брони сапог от 0 до 1:");
+        float bootsArmor = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Укажите минимальный урон оружия (0-20):");
+        float minDamage = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Укажите максимальный урон оружия (20-40):");
+        float maxDamage = float.Parse(Console.ReadLine());
+
+        Unit player = new Unit(name, health, new Helm(helmArmor), new Shell(shellArmor), new Boots(bootsArmor), new Weapon(minDamage, maxDamage));
+
+        Console.WriteLine("Общий показатель брони равен: " + player.Armor);
+        Console.WriteLine("Фактическое значение здоровья равно: " + player.RealHealth);
+
+        Console.ReadLine();
+    }
+}
---
+++ Classes_1/Unit.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public class Unit
+{
+    public Unit(string name, float health, Helm helm, Shell shell, Boots boots, Weapon weapon)
+    {
+        Name = name;
+        Health = health;
+        Helm = helm;
+        Shell = shell;
+        Boots = boots;
+        Weapon = weapon;
+    }
+
+    public string Name { get; }
+    private float _health;
+
+    public float Health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+        }
+    }
+
+    public int HealthInt
+    {
+        get
+        {
+            return (int)Math.Floor(Health);
+        }
+    }
+
+    public float Damage
+    {
+        get
+        {
+            return Weapon != null ? Weapon.Damage + BaseDamage : BaseDamage;
+        }
+    }
+
+    public float Armor
+    {
+        get
+        {
+            float HelmArmor = Helm != null ? Helm.ArmorValue : 0;
+            float ShellArmor = Shell != null ? Shell.ArmorValue : 0;
+            float BootsArmor = Boots != null ? Boots.ArmorValue : 0;
+
+            float TotalArmor = HelmArmor * ShellArmor * BootsArmor;
+
+            if (TotalArmor < 0)
+            {
+                TotalArmor = 0;
+            }
+            else if (TotalArmor > 1)
+            {
+                TotalArmor = 1;
+            }
+
+            return TotalArmor;
+        }
+    }
+
+    public float RealHealth
+    {
+        get
+        {
+            return Health * (1f + Armor);
+        }
+    }
+
+    public bool SetDamage(float value)
+    {
+        Health -= value * Armor;
+
+        return Health <= 0f;
+    }
+
+    private const float BaseDamage = 5;
+    public Weapon Weapon { get; set; }
+    public Helm Helm { get; set; }
+    public Shell Shell { get; set; }
+    public Boots Boots { get; set; }
+
+    public Unit() : this("Unknown Unit", 100)
+    {
+    }
+
+    public Unit(string name) : this(name, 100)
+    {
+    }
+
+    public Unit(string name, float health)
+    {
+        Name = name;
+        Health = health;
+    }
+
+    public void EquipWeapon(Weapon weapon)
+    {
+        Weapon = weapon;
+    }
+    public void EquipHelm(Helm helm)
+    {
+        Helm = helm;
+    }
+
+    public void EquipShell(Shell shell)
+    {
+        Shell = shell;
+    }
+
+    public void EquipBoots(Boots boots)
+    {
+        Boots = boots;
+    }
+}
